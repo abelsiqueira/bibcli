6b5a76cec07181f7c425ff977be80369509721f5
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-04-23 19:09:34 -0300

    In generate enable multiple tex files and remove duplicates

--- lib/bibcli/commands/generate.rb
+++ lib/bibcli/commands/generate.rb
@@ -10,31 +10,32 @@ module Bibcli
             return
           end
 
-          filename = args[0]
-          if File.exist?(filename)
-            src = File.open(filename).read().scan(/cite{([^}]+)}/)
-            refs = []
-            src.each { |x|
-              x[0].split(',').each { |a| refs << a.strip }
-            }
-
-            homebib = Bibcli::Parser.opendb(Bibcli::Interface.homefile)
-            refs.each { |key|
-              homebib.each { |type,info|
-                if homebib[type][key]
-                  puts "@#{type}{#{key},"
-                  output = []
-                  info[key].each { |k,v|
-                    output << "  #{k} = \"#{v}\""
-                  }
-                  puts output.join(",\n")
-                  puts "}\n\n"
-                end
+          refs = []
+          args.each do |filename|
+            if File.exist?(filename)
+              src = File.open(filename).read().scan(/cite{([^}]+)}/)
+              src.each { |x|
+                x[0].split(',').each { |a| refs << a.strip }
               }
-            }
+            end
           end
-        end
 
+          refs.uniq!
+          homebib = Bibcli::Parser.opendb(Bibcli::Interface.homefile)
+          refs.each { |key|
+            homebib.each { |type,info|
+              if homebib[type][key]
+                puts "@#{type}{#{key},"
+                output = []
+                info[key].each { |k,v|
+                  output << "  #{k} = \"#{v}\""
+                }
+                puts output.join(",\n")
+                puts "}\n\n"
+              end
+            }
+          }
+        end
       end
 
     end
