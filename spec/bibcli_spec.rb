ea7f606c84fa1b6c3884f492dccb3e2cea69b0d6
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-05-22 20:58:27 -0300

    Create a test for Parser.openbib

---
+++ spec/bibcli_spec.rb
@@ -0,0 +1,16 @@
+require 'bibcli'
+
+describe Bibcli do
+  describe Bibcli::Parser do
+    it "test if openbib works" do
+      testbib = Bibcli::Parser.openbib('spec/data/one.bib')
+      expect(testbib.keys).to eq([:article])
+      expect(testbib[:article].keys).to eq(["siqueira2014first"])
+      expected = {:author=>"Siqueira, Abel Soares",
+                  :title=>"First test",
+                  :year=>"2014",
+                  :journal=>"Test journal"}
+      expect(testbib[:article]["siqueira2014first"]).to eq(expected)
+    end
+  end
+end
