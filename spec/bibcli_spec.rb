9f51d1343ec62d45169bd498069b6eefe351eb46
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-05-22 21:06:18 -0300

    Add test for opendb

--- spec/bibcli_spec.rb
+++ spec/bibcli_spec.rb
@@ -12,5 +12,16 @@ describe Bibcli do
                   :journal=>"Test journal"}
       expect(testbib[:article]["siqueira2014first"]).to eq(expected)
     end
+
+    it "test if opendb works" do
+      testbib = Bibcli::Parser.opendb('spec/data/one.db')
+      expect(testbib.keys).to eq(["article"])
+      expect(testbib["article"].keys).to eq(["siqueira2014first"])
+      expected = {"author"=>"Siqueira, Abel Soares",
+                  "title"=>"First test",
+                  "year"=>"2014",
+                  "journal"=>"Test journal"}
+      expect(testbib["article"]["siqueira2014first"]).to eq(expected)
+    end
   end
 end
