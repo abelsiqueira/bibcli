ba2764dcf22dfd190db912baf0f17d5267997082
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-06-03 13:20:36 -0300

    Refactor add

--- lib/bibcli/commands/add.rb
+++ lib/bibcli/commands/add.rb
@@ -6,25 +6,29 @@ module Bibcli
 
         def process(args)
           if args[0] == "file"
-            filename = args[1]
-            bib = Bibcli::Parser.openbib(filename)
-            homebib = Bibcli::Parser.opendb(Bibcli::Interface.homefile)
-
-            bib.each { |type, value|
-              type = type.to_s
-              if homebib[type] == nil
-                homebib[type] = value
-              else
-                homebib[type].merge!(value)
-              end
-            }
-
-            open(Bibcli::Interface.homefile, 'w') { |file|
-              file.write(JSON.pretty_generate(homebib))
-            }
+            addfile(args[1])
           else
             raise "ERROR: command not found"
           end
+        end
+
+        def addfile(filename)
+          raise "ERROR: file #{filename} not found" unless File.exist?(filename)
+          bib = Bibcli::Parser.openbib(filename)
+          homebib = Bibcli::Parser.opendb(Bibcli::Interface.homefile)
+
+          bib.each { |type, value|
+            type = type.to_s
+            if homebib[type] == nil
+              homebib[type] = value
+            else
+              homebib[type].merge!(value)
+            end
+          }
+
+          open(Bibcli::Interface.homefile, 'w') { |file|
+            file.write(JSON.pretty_generate(homebib))
+          }
         end
 
       end
